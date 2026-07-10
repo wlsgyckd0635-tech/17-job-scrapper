7c4531798d968d6ab5362940cc622b710a9cb8ab
wlsgyckd0635 <wlsgyckd0635@gmail.com>
2026-07-10 14:17:21 +0900

    requirments.txt

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, send_file, redirect
+from scrapper import search_incruit
+from file import save_to_csv
+
+app = Flask(__name__)
+
+db = {}
+page = 5
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+
+    if keyword == "":
+        return redirect("/")
+    
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = search_incruit(keyword, page)
+        db[keyword] = jobs
+    jobs = search_incruit(keyword, page)
+    db[keyword] = jobs
+    return render_template("search.html", jobs=enumerate(jobs), keyword=keyword, count=len(jobs))
+
+@app.route("/file")
+def file():
+    keyword = request.args.get("keyword")
+
+    if keyword == "":
+        return redirect("/")
+    
+    if keyword not in db:
+        jobs = db[keyword]
+    else:
+        jobs = search_incruit(keyword, page)
+        
+        
+
+    jobs = search_incruit(keyword, page)
+    save_to_csv(jobs)
+    return send_file("./downloads.csv", as_attachment=True)
+
+    
+if __name__ == '__main__':
+    app.run()
